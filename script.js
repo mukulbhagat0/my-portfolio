7b6049b3fb06a54952b7557669e3ef30343d5d1b
mukulbhagat0 <159694778+mukulbhagat0@users.noreply.github.com>
2024-11-16 19:05:38 +0530

    created portflo

---
+++ script.js
@@ -0,0 +1,6 @@
+// Placeholder for form submission functionality
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Thank you for contacting me. I will get back to you soon!');
+  });
+  
